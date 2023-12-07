4cc8327cdcfc5e6708ff291308164938094c29ec
aa-shin <aa.shin@yahoo.com>
2023-12-07 03:08:35 -0500

    final

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,5 +1,38 @@
-const Authenticate = () => {
-  return <h2>Authenticate!</h2>;
+import { useState } from "react";
+
+const Authenticate = ({ token }) => {
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+
+  async function handleClick(event) {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/authenticate",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const result = await response.json();
+      setSuccessMessage(result.message);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <>
+      <h2>Authenticate</h2>
+      {successMessage && <p>{successMessage}</p>}
+      {error && <p>{error}</p>}
+      <button onClick={handleClick}>Authenticate Token!</button>
+    </>
+  );
 }
 
 export default Authenticate;
